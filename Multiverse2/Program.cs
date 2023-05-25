8a33067eb9a4314106efa73fd18800c8a8ef8c9f
Votre Nom Complet <j-francois_magdaline@hotmail.fr>
2023-05-25 10:51:01 +0200

    change display to World A

--- Multiverse2/Program.cs
+++ Multiverse2/Program.cs
@@ -6,7 +6,7 @@ namespace Multiverse2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Master!");
+            Console.WriteLine("Hello World A!");
         }
     }
 }
